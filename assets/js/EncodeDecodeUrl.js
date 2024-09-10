a684e0931aea711fa5d9078ce826854523df8a3c
emmma-mj <emmamariejeanne@gmail.com>
2024-09-10 22:21:26 +0400

    first commit

---
+++ assets/js/EncodeDecodeUrl.js
@@ -0,0 +1,15 @@
+﻿function EncodeUrl(url) {
+    return btoa(encode_utf8(url));
+}
+
+function DecodeUrl(url) {
+    return atob(decode_utf8(url));
+}
+
+function encode_utf8(s) {
+    return unescape(encodeURIComponent(s));
+}
+
+function decode_utf8(s) {
+    return decodeURIComponent(escape(s));
+}
